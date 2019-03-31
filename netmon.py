41e76ea64267c323458850097d42ed0ccfa4acfb
nvaldez <nolivaldeziii@gmail.com>
2019-04-01 07:42:54 +0800

    added more tests

--- netmon.py
+++ netmon.py
@@ -1,11 +1,14 @@
 import re
+import os
+import time
 
 class IpAddress:
     def __init__(self, value):
         self.Ip = value
-        self.uptime = 0
-        self.downtime = 0
-        self.time_last_success_ping = 0
+        self.Uptime = 0
+        self.Downtime = 0
+        self.Time_last_success_ping = 0
+        self.Is_Up = False
 
     def refresh(self):
         pass
@@ -13,13 +16,34 @@ class IpAddress:
 class Network:
     def __init__(self):
         self.Address = {}
+        self.Count = 0
 
-    def add_ip_address(self, name, IpObj):
+    def add_ip_address(self, name, IpObj, forced = False):
+        if name in self.Address and not forced:
+            raise self.DuplicateAddressException('Duplicate Network Name was found!')
+
         self.Address[name] = IpObj
+        self.Count = self.Count + 1
+
+    def ping_all(self):
+        for key in sorted(self.Address):
+            self.ping(key)
+
+    def ping(self, name):
+        self.Address[name].Is_Up = True if os.system('ping -n 1 -w 1 ' + self.Address[name].Ip + ' > nul') is 0 else False
+        return self.Address[name].Is_Up
+
+    class DuplicateAddressException(Exception):
+        """Raised if duplicate name has been added to the network"""
+        pass
 
 class Printer:
-    def __init__(self, IpAddresses):
+    def __init__(self):
         pass
+
+    def print(self, Network):
+        for key in sorted(Network.Address):
+                print( key + ' -> ' + Network.Address[key].Ip + ' is ' + str(Network.Address[key].Is_Up))
 
 class FileReader:
     def __init__(self,filename):
@@ -38,14 +62,21 @@ class FileReader:
 class Netmon:
     def __init__(self, IpFile):
         self.Network = Network()
+        self.Printer = Printer()
         IpListFile = FileReader(IpFile)
         for line in IpListFile.Lines:
             raw = line.split(',')
             self.Network.add_ip_address(raw[0],IpAddress(raw[1]))
 
-    def Run(self):
-        pass
+    def Run_Continuously(self):
+        while(True):
+            os.system('cls')
+            self.Run()
 
+    def Run(self):
+            self.Printer.print(self.Network)
+            self.Network.ping_all()
+            time.sleep(1)
 
 NetmonProgram = Netmon('ipaddress.txt')
-NetmonProgram.Run()
+NetmonProgram.Run_Continuously()
--- test_Network.py
+++ test_Network.py
@@ -7,8 +7,21 @@ class TestNetwork(unittest.TestCase):
         self.Network = Network()
 
     def test_add_ip_address(self):
-        self.Network.add_ip_address('home',IpAddress('128.0.0.1'))
-        self.assertEqual(self.Network.Address['home'].Ip,'128.0.0.1')
+        self.Network.add_ip_address('home',IpAddress('127.0.0.1'))
+        self.assertEqual(self.Network.Address['home'].Ip,'127.0.0.1')
+
+    def test_add_ip_address_duplicate(self):
+        self.Network.add_ip_address('home',IpAddress('127.0.0.1'))
+        self.assertRaises(self.Network.DuplicateAddressException, self.Network.add_ip_address, 'home',IpAddress('128.0.0.1'))
+
+
+    def test_ping_success(self):
+        self.Network.add_ip_address('home',IpAddress('127.0.0.1'))
+        self.assertTrue(self.Network.ping('home'))
+
+    def test_ping_fail(self):
+        self.Network.add_ip_address('not_home',IpAddress('128.0.0.1'))
+        self.assertFalse(self.Network.ping('not_home'))
 
 if __name__ == '__main__':
     unittest.main()
